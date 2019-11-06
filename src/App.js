49d43615e5ec7c93ae45666713b2c13f83536d87
cgonzaru <cristina.glez.rubio@gmail.com>
2019-11-06 12:56:59 +0100

    commit inicial

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app__header">
+        <h1 className="header__title">Reto front-end</h1>
+      </header>
+
+      <main className="app__main">
+        
+      </main>
+
+      <footer className="app__footer">
+        Cristina González Rubio &copy; 2019
+      </footer>
+    </div>
+  );
+}
+
+export default App;
